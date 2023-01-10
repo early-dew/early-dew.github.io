14d541be6e4ba5131c0583b411ace612143f5659
early-dew <72613584+early-dew@users.noreply.github.com>
2023-01-10 20:00:04 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,62 @@
+const navToggle = document.querySelector('.nav-toggle')
+const links = document.querySelector('.links')
+const linksContainer = document.querySelector(".links-container");
+const quoteOne = document.querySelector('.quote-container-one')
+const quoteTwo = document.querySelector('.quote-container-two')
+const navbar = document.getElementById("nav");
+
+
+navToggle.addEventListener("click", function () {
+  linksContainer.classList.toggle("show-links");
+    // let scrollHeight = window.pageYOffset;
+});
+
+window.addEventListener('scroll', function(){
+if (window.scrollY > 500) {
+linksContainer.classList.remove('show-links')
+}
+})
+
+
+// Quote circles
+
+window.addEventListener('scroll', function(){
+  if(this.window.innerWidth < 800 && window.scrollY > 200){
+    quoteOne.classList.add('slideUp')
+  }else if (window.scrollY >= 650) {quoteOne.classList.add('slideUp')}
+     })
+
+     window.addEventListener('scroll', function(){
+  if(this.window.innerWidth < 800 && window.scrollY > 2000){
+    quoteTwo.classList.add('slideUp')
+  }else if (window.scrollY >= 2400) {quoteTwo.classList.add('slideUp')}
+     })
+
+
+
+    //  Footer Date
+     const date = document.getElementById("date");
+date.innerHTML = new Date().getFullYear();
+
+
+const scrollLinks = document.querySelector('.scroll-link')
+
+  scrollLinks.addEventListener('click', function(e){
+  e.preventDefault();
+  
+  //navigate to specific spot
+  const id = e.currentTarget.getAttribute('href').slice(1);
+  const element = document.getElementById(id);
+  const navHeight = navbar.getBoundingClientRect().height;
+  const containerHeight = linksContainer.getBoundingClientRect().height
+  let position = element.offsetTop - navHeight;
+  if(navHeight > 82){
+    position = position + containerHeight;
+    
+  }
+  window.scrollTo({
+    left:0,top:position
+  })
+    })
+
+
---
+++ nft.js
@@ -0,0 +1,160 @@
+// import {finalGal} from './gallery.js';
+
+
+
+
+// Nav-bar
+const navToggle = document.querySelector('.nav-toggle')
+const links = document.querySelector('.links')
+const linksContainer = document.querySelector(".links-container");
+const quoteOne = document.querySelector('.quote-container-one')
+const quoteTwo = document.querySelector('.quote-container-two')
+const navbar = document.getElementById("nav");
+
+//Up-button
+const topLink = document.querySelector(".top-link");
+window.addEventListener('scroll', function(){
+ if (window.scrollY > 800) {
+
+    topLink.classList.add("show-link");
+  } else {
+    topLink.classList.remove("show-link");
+  }
+}); 
+
+
+navToggle.addEventListener("click", function () {
+  linksContainer.classList.toggle("show-links");
+    // let scrollHeight = window.pageYOffset;
+});
+window.addEventListener('scroll', function(){
+if (window.scrollY > 500) {
+linksContainer.classList.remove('show-links')
+}
+});
+
+
+
+// Gallery Modal
+
+function getElement(selection) {
+  const element = document.querySelector(selection);
+  if (element) {
+    return element;
+  }
+  throw new Error(
+    `Please check "${selection}" selector, no such element exists`
+  );}
+
+class Gallery {
+  constructor(element) {
+    this.container = element;
+    this.list = [...element.querySelectorAll('.images')];
+    // target
+    this.modal = getElement('.modal');
+    this.modalImg = getElement('.main-img');
+    this.imageName = getElement('.image-name');
+    this.modalImages = getElement('.modal-images');
+    this.closeBtn = getElement('.close-btn');
+    this.nextBtn = getElement('.next-btn');
+    this.prevBtn = getElement('.prev-btn');
+    // self ref
+    // let self = this;
+    // bind functions
+    // this.openModal = this.openModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+    this.nextImage = this.nextImage.bind(this);
+    this.prevImage = this.prevImage.bind(this);
+    this.chooseImage = this.chooseImage.bind(this);
+    // container event
+    this.container.addEventListener(
+      'click',
+      function (e) {
+        // self.openModal();
+        if (e.target.classList.contains('images')) {
+          this.openModal(e.target, this.list);
+        }
+      }.bind(this)
+    );
+  }
+  openModal(selectedImage, list) {
+    this.setMainImage(selectedImage);
+    this.modalImages.innerHTML = list
+      .map(function (image) {
+        return `<img src="${
+          image.src
+        }" title="${image.title}" data-id="${image.dataset.id}" class="${selectedImage.dataset.id === image.dataset.id ? 'modal-img selected' : 'modal-img'}"/>`;
+      })
+      .join('');
+    this.modal.classList.add('open');
+    this.closeBtn.addEventListener('click', this.closeModal);
+    this.nextBtn.addEventListener('click', this.nextImage);
+    this.prevBtn.addEventListener('click', this.prevImage);
+    this.modalImages.addEventListener('click', this.chooseImage);
+  }
+
+  setMainImage(selectedImage) {
+    this.modalImg.src = selectedImage.src;
+    this.imageName.textContent = selectedImage.title;
+  }
+
+  closeModal() {
+    this.modal.classList.remove('open');
+    this.closeBtn.removeEventListener('click', this.closeModal);
+    this.nextBtn.removeEventListener('click', this.nextImage);
+    this.prevBtn.removeEventListener('click', this.prevImage);
+    this.modalImages.removeEventListener('click', this.chooseImage);
+  }
+  nextImage() {
+    const selected = this.modalImages.querySelector('.selected');
+    const next =
+      selected.nextElementSibling || this.modalImages.firstElementChild;
+    selected.classList.remove('selected');
+    next.classList.add('selected');
+    this.setMainImage(next);
+  }
+  prevImage() {
+    const selected = this.modalImages.querySelector('.selected');
+    const prev =
+      selected.previousElementSibling || this.modalImages.lastElementChild;
+    selected.classList.remove('selected');
+    prev.classList.add('selected');
+    this.setMainImage(prev);
+  }
+  chooseImage(e) {
+    if (e.target.classList.contains('modal-img')) {
+      const selected = this.modalImages.querySelector('.selected');
+      selected.classList.remove('selected');
+
+      this.setMainImage(e.target);
+      e.target.classList.add('selected');
+    }
+  }
+}
+
+const finalGal = new Gallery(getElement('.final-gallery'));
+
+    //  Footer Date
+     const date = document.getElementById("date");
+date.innerHTML = new Date().getFullYear();
+
+
+const scrollLinks = document.querySelector('.scroll-link')
+
+  scrollLinks.addEventListener('click', function(e){
+  e.preventDefault();
+  
+  //navigate to specific spot
+  const id = e.currentTarget.getAttribute('href').slice(1);
+  const element = document.getElementById(id);
+  const navHeight = navbar.getBoundingClientRect().height;
+  const containerHeight = linksContainer.getBoundingClientRect().height
+  let position = element.offsetTop - navHeight;
+  if(navHeight > 82){
+    position = position + containerHeight;
+    
+  }
+  window.scrollTo({
+    left:0,top:position
+  })
+    })
